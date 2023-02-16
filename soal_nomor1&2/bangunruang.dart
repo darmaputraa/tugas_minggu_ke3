fe16a580dd0df23b2315c46789a68ccd3ee16b97
darmaputraa <124060650+darmaputraa@users.noreply.github.com>
2023-02-16 21:33:10 +0700

    Create bangunruang.dart

---
+++ soal_nomor1&2/bangunruang.dart
@@ -0,0 +1,18 @@
+class BangunRuang {
+  int panjang = 0;
+  int lebar = 0;
+  int tinggi = 0;
+  int sisi = 0;
+
+  BangunRuang() {
+    panjang = 0;
+    lebar = 0;
+    tinggi = 0;
+    sisi = 0;
+  }
+
+  int volume() {
+    print('Hitung Volume');
+    return 0;
+  }
+}
